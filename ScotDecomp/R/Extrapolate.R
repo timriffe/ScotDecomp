5adf9beadfa2e78515e16396f81c6694a6499320
Tim Riffe <tim.riffe@gmail.com>
2017-09-14 18:30:43 +0100

    extrapolation done

--- ScotDecomp/R/Extrapolate.R
+++ ScotDecomp/R/Extrapolate.R
@@ -4,3 +4,62 @@
 
 # a script to extrapolate mx from age 85+ to age 110+
 
+# read in data
+SCO <- read.csv("/home/tim/git/ScotDecomp/ScotDecomp/Data/lifetables_quintiles_scotland.csv")
+
+# get maxlik kannisto functions
+source("/home/tim/git/ScotDecomp/ScotDecomp/R/Functions.R")
+
+library(reshape2)
+
+DXm <- acast(SCO[SCO$sex == 1, ], age~year~quintile_2, value.var = "D")
+DXf <- acast(SCO[SCO$sex == 2, ], age~year~quintile_2, value.var = "D")
+NXm <- acast(SCO[SCO$sex == 1, ], age~year~quintile_2, value.var = "N")
+NXf <- acast(SCO[SCO$sex == 2, ], age~year~quintile_2, value.var = "N")
+DX <- DXm[, ,1]
+NX <- NXm[, ,1]
+
+
+extrap <- function(DX, NX){
+	DX <- DX[-nrow(DX), ]
+	NX <- NX[-nrow(NX), ]
+	mat110 <- matrix(NA,ncol=ncol(DX),nrow=111,dimnames=list(0:110,colnames(DX)))
+	DXe <- mat110
+	DXe[rownames(DX),colnames(DX)] <- DX
+	NXe <- mat110
+	NXe[rownames(NX),colnames(NX)] <- NX
+	
+	ages.i <- rep(nrow(DX),ncol(DX)) + 1
+	ltper_mx_v5(DXe,NXe,ages.i,fit.from.i = 76 )
+}
+
+Container.mx.m <- array(dim=c(111,dim(DXm)[2:3]),dimnames=list(0:110,seq(1981,2011,10),c(1:5,999)))
+Container.mx.f <- array(dim=c(111,dim(DXf)[2:3]),dimnames=list(0:110,seq(1981,2011,10),c(1:5,999)))
+
+# just ignore warnings and printouts
+for (i in 1:dim(DXm)[3]){
+	Container.mx.m[,,i] <- extrap(DXm[,,i],NXm[,,i])
+	Container.mx.f[,,i] <- extrap(DXf[,,i],NXf[,,i])
+}
+# reshape to long for easier operations
+mxmlong     <- melt(Container.mx.m, value.name="mx",varnames=c("age","year","quintile_2"))
+mxflong     <- melt(Container.mx.f, value.name="mx",varnames=c("age","year","quintile_2"))
+mxmlong$sex <- 1
+mxflong$sex <- 2
+SCOlong     <- rbind(mxmlong, mxflong)
+
+# now get back to qx
+SCOlong$ax                     <- .5
+# infants, same as SCO
+SCOlong$ax[SCOlong$age == 0]   <- .1
+# constant hazard closeout assumption
+SCOlong$ax[SCOlong$age == 110] <- 1 / SCOlong$mx[SCOlong$age == 110] 
+
+# now get qx:
+SCOlong$qx <- SCOlong$mx / (1 + (1- SCOlong$ax) * SCOlong$mx )
+# closeout:
+SCOlong$qx[SCOlong$age == 110] <- 1
+
+# quick check to see if anything stands out:
+# matplot(0:110,acast(SCOlong, age~year+quintile_2+sex,value.var="mx"),log='y',type='l')
+
--- ScotDecomp/R/Functions.R
+++ ScotDecomp/R/Functions.R
@@ -110,3 +110,194 @@ Vbetween <- function(QXk, pik = rep(1/ncol(QXk),ncol(QXk))){
 	Left - Right
 }
 
+# ------------------------------------------------------------
+# these are function used for extrapolation:
+
+# ---------------------------------------------------
+# define internal functions for kannisto maxlik
+# ---------------------------------------------------
+
+# Kannisto MLE fitting functions as of MP version 5
+# Eq 53, pg 36 MPv5
+# a,b, are unconstrained in these functions, since we can set bounds with L-BFGS-B method.
+KannistoMu <- function(pars, x = .5:30.5){
+	a        <- pars["a"]
+	b        <- pars["b"]
+	(a * exp(b * x)) / (1 + a * exp(b * x))
+}
+
+# likelihood, parameters constrained to positive using abs() only
+# This is actually partial log likelihood, which matters when getting the information
+# from the Hessian
+KannistoLik <- function(pars, .Dx, .Exp, .x. = .5:30.5){
+	mu       <- KannistoMu(pars, x = .x.)
+	# take negative and minimize it (default optimizer behavior)
+	-sum(.Dx * log(mu) - .Exp * mu, na.rm = TRUE)
+}
+
+# vector of partials with respect to a and b, respectively
+KannistoGr <- function(pars, .Dx, .Exp, .x. = .5:30.5){
+	a   <- pars["a"]
+	b   <- pars["b"]
+	d.a <- (a * exp(b * .x.) * .Exp + (-a * exp(b * .x.) - 1) * .Dx) /
+			(a ^ 3 * exp(2 * b * .x.) + 2 * a ^ 2 * exp(b * .x.) + a)
+	d.b <- (a * .x. * exp(b * .x.) * .Exp + (-a * .x. * exp(b * .x.) - .x.) * .Dx) /
+			(a ^ 2 * exp(2 * b * .x.) + 2 * a * exp(b * .x.) + 1)
+	colSums(cbind(a = d.a, b = d.b), na.rm = TRUE) 
+}
+
+# starting value finder, since sometimes the L-BFGS-L can fail to converge if it goes off
+# in the wrong direction. unfortunately. 
+startingValueGridSearch <- function(
+		a.vals = seq(.001, .701, by = .1),
+		b.vals = seq(.001, .701, by = .1), 
+		.Exp, .Dx, .x. = .5:30.5, i){
+	#a.vals = seq(.001, .701, by = .1)
+	#b.vals = seq(.001, .701, by = .1)   
+	# vectors of length = len(a)xlen(b) -- vectorized grid
+	ab.grid <- expand.grid(a.vals, b.vals)
+	a.vec <- ab.grid[, 1]
+	b.vec <- ab.grid[, 2]
+	
+	valsLik <- sapply(seq(along=a.vec), 
+			function(.i){ KannistoLik(c(a = a.vec[.i], b = b.vec[.i]), 
+						.Dx = .Dx, .Exp = .Exp, .x. = .x.)} )
+	# MP.likM <- matrix(ncol = length(a.vals), nrow = length(b.vals))
+	# for (a. in 1:length(a.vals)){
+	#   for (b. in 1:length(b.vals)){
+	#     MP.likM[b., a.] <- KannistoLik(c(a = a.vals[a.], b = b.vals[b.]), 
+	#       .Dx = .Dx, .Exp = .Exp, .x. = .x.)                     
+	#   } 
+	# }
+	arg.min <- which.min(valsLik)
+	# c(a = a.vals[col(MP.likM)[which.min(MP.likM)]], 
+	#   b = b.vals[row(MP.likM)[which.min(MP.likM)]])
+	
+	return( c(a = a.vec[arg.min], b = b.vec[arg.min]) )
+}
+
+
+
+ltper_mx_v5 <- function(Dx, Exp, extrap.ages.i = 81, extrap.to.i = 111, fit.from.i = 81){
+	
+	# -------------------------------------------------------------
+	# begin actual fitting 
+
+	ages    <- as.integer(rownames(Dx))
+	ages.i  <- fit.from.i:extrap.to.i - fit.from.i + .5
+	years   <- colnames(Dx)
+	par.est <- matrix(nrow = length(years), ncol = 2, dimnames = list(years, c("a","b")))
+	se.est  <- matrix(nrow = length(years), ncol = 2, dimnames = list(years, c("a","b")))
+	
+	# in a for-loop because we're iterating over 2 items..Dx and Exp
+	by.j    <- c(.1, .05, .02, .005, .001, .0002, .00005, .00001) # last entry, nr 8, is moot. 
+	# I think all cases converge .001,
+	
+	for (i in 1:ncol(Dx)){ # iterate over years
+		if(!all(is.na(Dx[,i]))){  # added for BEL
+			# the first set of starting values typically suffices. It's over a wide range, but quite rough
+			afromto.j <- c(.001, .701)
+			bfromto.j <- c(.001, .701)
+			# intitial starting values
+			starts.j  <- startingValueGridSearch(a.vals = seq(afromto.j[1], afromto.j[2], by = by.j[1]),
+					b.vals = seq(bfromto.j[1], bfromto.j[2], by = by.j[1]), 
+					.Dx = Dx[81:nrow(Dx), i], 
+					.Exp = Exp[81:nrow(Exp), i],
+					.x. = ages.i )
+			
+			# jump into optimization loop- keep trying better starts until it converges
+			
+			for (j in 1:7){ # j <- 1
+				# try optimizing with present starts
+				result <- try(optim(
+								starts.j, 
+								fn = function(...) KannistoLik(...) , # likelihood function rescaled in the optim call
+								gr =  function(...) KannistoGr(...) , # gradient function rescaled in the optim call
+								method = c("L-BFGS-B"),                     # BFGS with par constraints
+								upper = c(5, 5), lower = c(0, 0),           # upper, lower bounds for a,b
+								control = list(lmm = 20, fnscale=1.0e6, trace=0), hessian=TRUE,   # precision
+								.Dx = Dx[fit.from.i:nrow(Dx), i], .Exp = Exp[fit.from.i:nrow(Exp), i], .x. = ages.i # args to pass in
+						), 
+						silent = FALSE
+				);
+				
+				# if it didn't converge, an error will be thrown of class 'try-error'
+				if (class(result) == "try-error" || result$convergence > 0){
+					##cat(result$message, "\n\n")  # what went wrong?
+					## try again, but with a different fnscale, 
+					result <- try(optim(
+									starts.j, 
+									fn = function(...) KannistoLik(...) , # likelihood function rescaled in the optim call
+									gr =  function(...) KannistoGr(...) , # gradient function rescaled in the optim call
+									method = c("L-BFGS-B"),                     # BFGS with par constraints
+									upper = c(5, 5), lower = c(0, 0),           # upper, lower bounds for a,b
+									control = list(lmm = 20, fnscale=1.0e2, trace=2), hessian=TRUE,   # precision
+									.Dx = Dx[fit.from.i:nrow(Dx), i], .Exp = Exp[fit.from.i:nrow(Exp), i], .x. = ages.i # args to pass in
+							), 
+							silent = FALSE
+					);
+				}
+				
+				
+				# if it still didn't  converge, refine the grid search and retry
+				if (class(result) == "try-error" || result$convergence > 0){
+					##cat(result$message, "\n\n")  # what went wrong?
+					# in this case we select from a finer grid of starting values 
+					# within narrower limits and try again ('smart' grid search)
+					afromto.j <- c(max(starts.j["a"] - 2 * by.j[j], 0), starts.j["a"] + 2 * by.j[j])
+					bfromto.j <- c(max(starts.j["b"] - 2 * by.j[j], 0), starts.j["b"] + 2 * by.j[j])
+					starts.j  <- try(startingValueGridSearch(a.vals = seq(afromto.j[1], afromto.j[2], by = by.j[j + 1]),
+									b.vals = seq(bfromto.j[1], bfromto.j[2], by = by.j[j + 1]), 
+									.Dx = Dx[fit.from.i:nrow(Dx), i], 
+									.Exp = Exp[fit.from.i:nrow(Exp), i], .x. = ages.i))
+					next
+				} else {
+					## return parameter estimates and their SE
+					ab.j <- result$par
+					# stats.stackexchange.com:   "in R given an output from optim  hessian how to calculate parameter confidence intervals"
+					fisher_info <- solve( result$hessian )
+					se.j  <-  sqrt(diag(fisher_info))
+					break
+				}       
+			} # j
+			
+			# it only gets this far in extreme testing, allow for sake of simulations
+			if (class(result) == "try-error"){
+				ab.j <- starts.j
+				se.j <- c(NA,NA)
+			}
+			
+		} else {
+			ab.j <- c(NA, NA) +1  # class should be numeric
+			se.j <- c(NA, NA)
+		}
+		
+		par.est[i, ] <- ab.j    
+		se.est[i,]   <- se.j
+		cat(paste(i, ab.j[1], ab.j[2], "\n"))
+	} #i
+	
+	# fitted values over entire age range
+	mx.est  <- apply(par.est, 1, function(parsest){
+				KannistoMu(parsest, x = ages - fit.from.i + 1.5)
+			}
+	)
+	fit.from.i
+	# additional redundant check for parameters:
+	if (class(par.est[,1]) != "numeric" | class(par.est[,2]) != "numeric"){
+		stop("looks like one of the parameters didn't converge properly, check: ", sex, ctry)
+	}
+	
+	# raw Mx
+	mx <- Dx / Exp
+	# impute according to 'extrap.ages.i', which is supplied as such
+	for (i in seq_along(years)){
+		if (!all(is.na(mx[,i]))){ # added for BEL
+			ind1        <- extrap.ages.i[i]:nrow(mx)
+			mx[ind1, i] <- mx.est[ind1, i]
+		}
+	}
+	# return the finished mx
+	#list(mx=mx, par=par.est,  se=se.est)
+	return(mx)
+}
